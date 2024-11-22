c2634eb3d75e25f59f866262bb877162343d1bf3
Naito <naito01030103@icloud.com>
2024-11-22 18:31:04 +0900

    アニメーション変更

--- src/templates/home/Top.tsx
+++ src/templates/home/Top.tsx
@@ -6,6 +6,7 @@ import ProductHowItWorks from "../../components/home/top/ProductHowItWork";
 import ProductCategories from "../../components/home/top/ProductCategories";
 import ProductHowItWork from "../../components/home/top/ProductHowItWorks";
 import ProductCTA from "../../components/home/top/ProductCTA";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
@@ -19,10 +20,42 @@ function Home() {
       >
         <Header />
         <ProductHero />
-        <ProductHowItWork />
-        <ProductCategories />
-        <ProductHowItWorks />
-        <ProductCTA />
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+          whileInView={{
+            opacity: 1,
+          }}
+        >
+          <ProductHowItWork />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+          whileInView={{
+            opacity: 1,
+          }}
+        >
+          <ProductCategories />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+          whileInView={{
+            opacity: 1,
+          }}
+        >
+          <ProductHowItWorks />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+          whileInView={{
+            opacity: 1,
+          }}
+        >
+          <ProductCTA />
+        </motion.div>
         <Box component="footer" sx={{ mt: "auto", width: "100%" }}>
           <Footer />
         </Box>
