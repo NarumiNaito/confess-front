72ae4e8e9e8cae1bcf0494be1ace45a7ac9a76b4
Naito <naito01030103@icloud.com>
2024-12-03 03:03:21 +0900

    ルーティング修正

--- src/router/PublicLayout.tsx
+++ src/router/PublicLayout.tsx
@@ -1,10 +1,5 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuthContext } from "./useAuthContext";
+import { Outlet } from "react-router-dom";
 
 export default function PublicLayout() {
-  const { loggedIn } = useAuthContext();
-  if (loggedIn) {
-    return <Navigate to="/myPage" />;
-  }
   return <Outlet />;
 }
